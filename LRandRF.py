71f86eb71c066a9771795c5763309c7cbe57ab0b
Maha-coder-cmd <mahnoorbhatti4799@gmail.com>
2025-08-25 11:59:56 +0500

    Converted strings into numbers

--- LRandRF.py
+++ LRandRF.py
@@ -17,13 +17,20 @@ df.head()
 
 print(df.head())
 
+df_clean = pd.get_dummies(df_clean, columns=['prefarea', 'furnishingstatus'])
+df_clean.head()
+
 # Assuming 'X' = features,'y' = target
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# X = pd.get_dummies(df.drop('price', axis=1))
-# df['column_name'] = df['column_name'].map({'yes': 1, 'no': 0}) 
+print(df['prefarea'].unique())
+df['prefarea'] = df['prefarea'].map({'yes':1, 'no': 0})
 
-# #Train model
+
+print(df['furnishingstatus'].unique())
+df = pd.get_dummies(df, columns=['furnishingstatus'])
+
+#Train model
 # model = LinearRegression()
 # model.fit(X_train, y_train)
 
